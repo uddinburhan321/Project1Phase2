d8bcadc59d18b0a79e6c4a9fce1755fb657ae7f6
uddinburhan321 <burhan.uddin@mail.citytech.cuny.edu>
2016-04-11 18:14:26 -0400

    rename

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160406233224) do
+ActiveRecord::Schema.define(version: 20160406233433) do
 
   create_table "pages", force: :cascade do |t|
     t.integer  "visits"
