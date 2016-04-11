b60386662853bcc9773a48daf4abe8a38aa85548
uddinburhan321 <burhan.uddin@mail.citytech.cuny.edu>
2016-04-11 18:56:53 -0400

    rename

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,3 +1,11 @@
 class Page < ActiveRecord::Base
-  belongs_to :site
+	belongs_to :site
+	def average_duration
+		
+		if visits != 0
+			return self.duration/self.visits
+		else
+			return 0
+		end
+	end
 end
